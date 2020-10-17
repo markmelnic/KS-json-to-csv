446ee613392f9fc7faee1ae55ac9481ce7a837f7
Mark Melnic <markhappy200@gmail.com>
2020-10-17 11:23:31 +0200

    Generate payload method initiated

--- main.py
+++ main.py
@@ -15,3 +15,44 @@ if __name__ == "__main__":
                 payload = gen_payload(data)
                 print(payload)
                 break
+
+
+def gen_payload(data: str) -> list:
+    payload = [
+        date,
+        case_number,
+        county,
+        precinct,
+        disposition,
+        data["DispositionDate"],
+        data["DispositionAmount"],
+        data["Prefix"],
+        data["BaseCase"],
+        data["Suffix"],
+        data["Defendants"]["DefendantName"],
+        data["Defendants"]["SSN"],
+        data["Defendants"]["Dob"],
+        data["Defendants"]["DriversLicense"],
+        data["Defendants"]["DefAddress"],
+        data["Defendants"]["DefUnit"],
+        data["Defendants"]["DefCity"],
+        data["Defendants"]["DefState"],
+        data["Defendants"]["DefZip"],
+        data["Defendants"]["DefendantNameSkip"],
+        data["Plaintiffs"]["PltfNameInfo"],
+        data["Plaintiffs"]["PltfName"],
+        data["Plaintiffs"]["InCareOfName"],
+        data["Plaintiffs"]["PltfAddress"],
+        data["Plaintiffs"]["PltfCity"],
+        data["Plaintiffs"]["PltfState"],
+        data["Plaintiffs"]["PltfZip"],
+        data["Plaintiffs"]["PltfPhone"],
+        data["Plaintiffs"]["PltfCounty"],
+        data["AttorneyName"],
+        data["AttorneyPhone"],
+        data["AttorneyAddress"],
+        data["AttorneyCity"],
+        data["AttorneyState"],
+        data["AttorneyZip"],
+        data["attorneycounty"],
+    ]
