194c3e139aeb5c9d535c93c985636244b227be12
Mark Melnic <markhappy200@gmail.com>
2020-10-17 11:40:19 +0200

    Added general payload elements

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 import os, csv, json
 from settings import *
 
-if __name__ == "__main__":
+def main() -> None:
     with open("out.csv", "w") as out_csv:
         wr = csv.writer(out_csv)
         wr.writerow(FIRST_ROW)
-
         for entry in os.scandir("./"):
             if ".json" in entry.name:
                 with open(entry.name) as json_file:
@@ -18,41 +17,90 @@ if __name__ == "__main__":
 
 
 def gen_payload(data: str) -> list:
+    # date
+    date = data["FiledDate"]
+    # case number
+    case_number = data["CaseNumber"]
+    # county
+    county = data["County"]
+    # precinct
+    try:
+        precinct = data["Precinct"]
+    except KeyError:
+        precinct = ""
+    # disposition
+    try:
+        disposition = data["Disposition"]
+    except KeyError:
+        disposition = ""
+    # disposition date
+    try:
+        disposition_date = data["Disposition"]
+    except KeyError:
+        disposition_date = ""
+    # disposition amount
+    try:
+        disposition = data["Disposition"]
+    except KeyError:
+        disposition_amount = ""
+    # prefix
+    prefix = '-'.join(case_number.split("-")[:-1]) + "-"
+    for c in str(case_number.split("-")[-1]):
+        if c == '0':
+            prefix += '0'
+        else:
+            break
+    # base case
+    base_case = case_number.split("-")[-1]
+    for c in base_case:
+        if c == '0':
+            base_case = base_case[1:]
+        else:
+            break
+    # suffix
+    try:
+        suffix = data["Suffix"]
+    except KeyError:
+        suffix = ""
     payload = [
         date,
         case_number,
         county,
         precinct,
         disposition,
-        data["DispositionDate"],
-        data["DispositionAmount"],
-        data["Prefix"],
-        data["BaseCase"],
-        data["Suffix"],
-        data["Defendants"]["DefendantName"],
-        data["Defendants"]["SSN"],
-        data["Defendants"]["Dob"],
-        data["Defendants"]["DriversLicense"],
-        data["Defendants"]["DefAddress"],
-        data["Defendants"]["DefUnit"],
-        data["Defendants"]["DefCity"],
-        data["Defendants"]["DefState"],
-        data["Defendants"]["DefZip"],
-        data["Defendants"]["DefendantNameSkip"],
-        data["Plaintiffs"]["PltfNameInfo"],
-        data["Plaintiffs"]["PltfName"],
-        data["Plaintiffs"]["InCareOfName"],
-        data["Plaintiffs"]["PltfAddress"],
-        data["Plaintiffs"]["PltfCity"],
-        data["Plaintiffs"]["PltfState"],
-        data["Plaintiffs"]["PltfZip"],
-        data["Plaintiffs"]["PltfPhone"],
-        data["Plaintiffs"]["PltfCounty"],
-        data["AttorneyName"],
-        data["AttorneyPhone"],
-        data["AttorneyAddress"],
-        data["AttorneyCity"],
-        data["AttorneyState"],
-        data["AttorneyZip"],
-        data["attorneycounty"],
+        disposition_date,
+        disposition_amount,
+        prefix,
+        base_case,
+        suffix,
+        #data["Defendants"]["DefendantName"],
+        #data["Defendants"]["SSN"],
+        #data["Defendants"]["Dob"],
+        #data["Defendants"]["DriversLicense"],
+        #data["Defendants"]["DefAddress"],
+        #data["Defendants"]["DefUnit"],
+        #data["Defendants"]["DefCity"],
+        #data["Defendants"]["DefState"],
+        #data["Defendants"]["DefZip"],
+        #data["Defendants"]["DefendantNameSkip"],
+        #data["Plaintiffs"]["PltfNameInfo"],
+        #data["Plaintiffs"]["PltfName"],
+        #data["Plaintiffs"]["InCareOfName"],
+        #data["Plaintiffs"]["PltfAddress"],
+        #data["Plaintiffs"]["PltfCity"],
+        #data["Plaintiffs"]["PltfState"],
+        #data["Plaintiffs"]["PltfZip"],
+        #data["Plaintiffs"]["PltfPhone"],
+        #data["Plaintiffs"]["PltfCounty"],
+        #data["AttorneyName"],
+        #data["AttorneyPhone"],
+        #data["AttorneyAddress"],
+        #data["AttorneyCity"],
+        #data["AttorneyState"],
+        #data["AttorneyZip"],
+        #data["attorneycounty"],
     ]
+    return payload
+
+if __name__ == "__main__":
+    main()
